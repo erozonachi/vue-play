5707f34582eeb90d0ed1894023cd4b3aa2c5ba18
erozonachi <erozonachi.james@gmail.com>
2019-09-15 00:55:13 +0100

    Create and export a todo item component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>{{ todo.text }}</li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    todo: Object
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
