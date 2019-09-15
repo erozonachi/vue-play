0fd61eb7d7ea94a5f93f25cb899db851bf1d5f19
erozonachi <erozonachi.james@gmail.com>
2019-09-15 01:18:19 +0100

    Update app component to render todo items

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ul>
+    <TodoItem v-for="item in groceryList" v-bind:todo="item" v-bind:key="item.id" />
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    TodoItem
+  },
+  data: () => ({
+    groceryList: [
+      { id: 0, text: "Vegetables" },
+      { id: 1, text: "Cheese" },
+      { id: 2, text: "Whatever else humans are supposed to eat" }
+    ]
+  })
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
